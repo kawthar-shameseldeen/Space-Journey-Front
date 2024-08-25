70f7926fc4e0d34545d81a4dc4a78879f7d1086c
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 00:16:36 +0300

    corrected import

--- space-journey/src/pages/home/home.jsx
+++ space-journey/src/pages/home/home.jsx
@@ -2,10 +2,10 @@ import { useState , useEffect } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import "./home.css";
-import Navbar from "../../compnents/navbar/navbar.jsx";
-import AboutUs from "../../compnents/aboutus/aboutus.jsx";
-import Features from "../../compnents/features/features.jsx";
-import Footer from "../../compnents/footer/footer.jsx";
+import Navbar from "../../components/navbar/navbar.jsx";
+import AboutUs from "../../components/aboutus/aboutus.jsx";
+import Features from "../../components/features/features.jsx";
+import Footer from "../../components/footer/footer.jsx";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
