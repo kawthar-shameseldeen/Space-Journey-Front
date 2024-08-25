d5bd5c13b8e2c02e307edf1b5fcfe3af7df895d5
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-25 23:33:08 +0300

    imported redux file

--- space-journey/src/pages/signup/signup.jsx
+++ space-journey/src/pages/signup/signup.jsx
@@ -1,3 +1,9 @@
 import { FaUser, FaEnvelope, FaLock } from 'react-icons/fa';
 import './signup.css';
 import { useDispatch, useSelector } from "react-redux";
+import {
+    registerUserSuccess,
+    registerUserStart,
+    registerUserFail,
+    usersSliceSelector,
+  } from "../../data_store/redux/userSlice/index.js";
