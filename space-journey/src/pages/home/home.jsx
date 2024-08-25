da87f87aaa9c46a5471ea6e657813c3184ab2726
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 01:45:37 +0300

    fixed bug

--- space-journey/src/pages/home/home.jsx
+++ space-journey/src/pages/home/home.jsx
@@ -9,130 +9,130 @@ import Footer from "../../components/footer/footer.jsx";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-    const particlesInit = async (main) => {
-      await loadFull(main);
-    };
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const navigate=useNavigate();
-    useEffect(() => {
-    
-      const token = localStorage.getItem("token");
-      if (token) {
-        setIsAuthenticated(true);
-      }
-    }, []);
-    const handleLogout = () => {
-      localStorage.removeItem("token");
-      setIsAuthenticated(false);
-      navigate("/login");
-  
-    };
-    return (
-      <div className="home-container">
-        <Particles
-          id="tsparticles"
-          init={particlesInit}
-          options={{
-            fpsLimit: 60,
-            particles: {
-              number: {
-                value: 1000,
-                density: {
-                  enable: true,
-                  value_area: 800,
-                },
-              },
-              color: {
-                value: "#61dbfb",
-              },
-              shape: {
-                type: "circle",
-              },
-              opacity: {
-                value: 3,
-                anim: {
-                  enable: true,
-                  speed: 2,
-                  opacity_min: 0.1,
-                  sync: false,
-                },
-              },
-              size: {
-                value: 3,
-                anim: {
-                  enable: true,
-                  speed: 5,
-                  size_min: 0.3,
-                  sync: false,
-                },
-              },
-              move: {
-                enable: true,
-                speed: 1,
-                direction: "none",
-                random: false,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: {
-                  enable: false,
-                },
-              },
-            },
-            interactivity: {
-              detectsOn: "canvas",
-              events: {
-                onHover: {
-                  enable: true,
-                  mode: "repulse",
-                },
-                onClick: {
-                  enable: true,
-                  mode: "push",
-                },
-                resize: true,
-              },
-              modes: {
-                repulse: {
-                  distance: 50,
-                  duration: 0.4,
-                },
-                push: {
-                  particles_nb: 4,
-                },
-                remove: {
-                  particles_nb: 2,
-                },
-              },
-            },
-            retina_detect: true,
-            background: {
-              color: "#000000",
-            },
-            fullScreen: {
-              enable: true,
-              zIndex: -1, 
-            },
-          }}
-        />
-        <Navbar isAuthenticated={isAuthenticated} onLogout={handleLogout} />
-        <section className="hero-section">
-          <div className="content">
-            <h1>Hello Universe</h1>
-          </div>
-        </section>
-        <div className="cards-section">
-          <div className="card-container">
-            <AboutUs />
-          </div>
-          <div className="card-container">
-            <Features />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
+  const particlesInit = async (main) => {
+    await loadFull(main);
   };
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate=useNavigate();
+  useEffect(() => {
   
-  export default HomePage;
-  
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+    navigate("/login");
+
+  };
+  return (
+    <div className="home-container">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          fpsLimit: 60,
+          particles: {
+            number: {
+              value: 1000,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: "#61dbfb",
+            },
+            shape: {
+              type: "circle",
+            },
+            opacity: {
+              value: 3,
+              anim: {
+                enable: true,
+                speed: 2,
+                opacity_min: 0.1,
+                sync: false,
+              },
+            },
+            size: {
+              value: 3,
+              anim: {
+                enable: true,
+                speed: 5,
+                size_min: 0.3,
+                sync: false,
+              },
+            },
+            move: {
+              enable: true,
+              speed: 1,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+              },
+            },
+          },
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              repulse: {
+                distance: 50,
+                duration: 0.4,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
+          },
+          retina_detect: true,
+          background: {
+            color: "#000000",
+          },
+          fullScreen: {
+            enable: true,
+            zIndex: -1, 
+          },
+        }}
+      />
+      <Navbar isAuthenticated={isAuthenticated} onLogout={handleLogout} />
+      <section className="hero-section">
+        <div className="content">
+          <h1>Hello Universe</h1>
+        </div>
+      </section>
+      <div className="cards-section">
+        <div className="card-container">
+          <AboutUs />
+        </div>
+        <div className="card-container">
+          <Features />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
