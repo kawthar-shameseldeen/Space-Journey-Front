27bd9ee57808fc03477c7298e5090d6550fdd09a
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-25 23:17:53 +0300

    added fileRader method

--- space-journey/src/components/fileuploader/fileuploader.jsx
+++ space-journey/src/components/fileuploader/fileuploader.jsx
@@ -17,7 +17,34 @@ const FileUploadPopup = ({ onFileImport }) => {
         const fileReader = new FileReader();
         const fileType = selectedFile.name.split('.').pop().toLowerCase();
     
-       
+        fileReader.onload = (e) => {
+          if (fileType === 'csv') {
+            Papa.parse(selectedFile, {
+              header: true,
+              skipEmptyLines: true,
+              complete: (results) => {
+                console.log("Parsed CSV Data:", results.data);
+                setParsedData(results.data);
+              },
+              error: (error) => {
+                toast.error("Error parsing CSV file");
+                console.error("Error parsing CSV file:", error);
+              }
+            });
+          } else if (fileType === 'xlsx') {
+            const binaryStr = e.target.result;
+            const workbook = XLSX.read(binaryStr, { type: 'binary' });
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const data = XLSX.utils.sheet_to_json(sheet);
+            console.log("Parsed XLSX Data:", data);
+            setParsedData(data);
+          } else {
+            toast.error("Unsupported file type");
+          }
+        };
+    
+      
       };
     
      
