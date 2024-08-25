d5f9cdaeb2b9cccfa6d8cdf6b2d8538123d2d684
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-25 22:44:01 +0300

    added footer

--- space-journey/src/components/footer/footer.jsx
+++ space-journey/src/components/footer/footer.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './footer.css';
 import { Link } from 'react-router-dom';
 const Footer = () => {
+    return (
+        <footer className="footer">
+           
+        </footer>
+    );
     
 };
 
