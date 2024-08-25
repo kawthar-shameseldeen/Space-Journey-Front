9ba49c67fc7eeac09a64890194765fb7d0ee91d2
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-25 22:46:40 +0300

    added footer logo

--- space-journey/src/components/footer/footer.jsx
+++ space-journey/src/components/footer/footer.jsx
@@ -9,7 +9,13 @@ const Footer = () => {
                     <a href="#contact">Contact Us</a>
                     <a href="#about">About Us</a>
                 </div>
-               
+                <div className="footer-logo">
+                    <h2>SpaceJourney</h2>
+                </div>
+                <div className="footer-links">
+                    <a href="#features"><Link to="/features">Features</Link></a>
+                    <a href="#terms">Terms of Use</a>
+                </div>
             </div>
            
         </footer>
