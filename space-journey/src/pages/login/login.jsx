0637abf1addcb3c419cdd0cd2a3f92d009b83623
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-25 23:54:51 +0300

    added form

--- space-journey/src/pages/login/login.jsx
+++ space-journey/src/pages/login/login.jsx
@@ -11,43 +11,44 @@ import {
     errorOccured,
   } from "../../data_store/redux/userSlice/index.js";
   const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-  
-    const handleEmailChange = (e) => setEmail(e.target.value);
-    const handlePasswordChange = (e) => setPassword(e.target.value);
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        if (!email || !password) {
-          toast.error("Please enter both email and password");
-          return;
-        }
-        dispatch(fetchingUsers());
-        const { data } = await axios.post("http://localhost:4040/api/login", {
-          email,
-          password,
-        });
-        dispatch(loadUsers(data));
-        const token = data.token;
-        localStorage.setItem("token", token);
-        toast.success("Login successful");
-        navigate("/home");
-      } catch (error) {
-        dispatch(errorOccured(error?.message));
-        toast.error("Error logging in");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (!email || !password) {
+        toast.error("Please enter both email and password");
+        return;
       }
-    };
-  
-    return (
-      <div className="login-container">
- 
-    </div>
-    );
+      dispatch(fetchingUsers());
+      const { data } = await axios.post("http://localhost:4040/api/login", {
+        email,
+        password,
+      });
+      dispatch(loadUsers(data));
+      const token = data.token;
+      localStorage.setItem("token", token);
+      toast.success("Login successful");
+      navigate("/home");
+    } catch (error) {
+      dispatch(errorOccured(error?.message));
+      toast.error("Error logging in");
+    }
   };
-  
-  export default Login;
-  
+
+  return (
+    <div className="login-container">
+    <form className="login" onSubmit={handleSubmit}>
+     
+    </form>
+  </div>
+  );
+};
+
+export default Login;
