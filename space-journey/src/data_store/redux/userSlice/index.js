7eee446d0bb8502b9e204457473f3b820ff6b1c3
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 01:46:24 +0300

    added userSlice

---
+++ space-journey/src/data_store/redux/userSlice/index.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    loading: false,
+    list: [],
+    error: null,
+    user: null,
+  };
+
+  const usersSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+      fetchingUsers: (state) => {
+        state.loading = true;
+      },
+      loadUsers: (state, action) => {
+        const { payload } = action;
+        state.loading = false;
+        state.list = payload;
+      },
+      errorOccured: (state, action) => {
+        const { payload } = action;
+        state.loading = false;
+        state.error = payload;
+      },
+      registerUserSuccess: (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      },
+      registerUserStart: (state) => {
+        state.loading = true;
+        state.error = null;
+      },
+      registerUserFail: (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      },
+    },
+  });
+  
+export const {
+    loadUsers,
+    fetchingUsers,
+    errorOccured,
+    registerUserSuccess,
+    registerUserStart,
+    registerUserFail,
+  } = usersSlice.actions;
+  export const usersSliceSelector = (state) => state.users;
+  export const usersReducer = usersSlice.reducer;
