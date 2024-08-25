4bab49683549b728ad30ea02787ced0035d9d62b
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-25 21:17:10 +0300

    created nav

--- space-journey/src/components/navbar/navbar.jsx
+++ space-journey/src/components/navbar/navbar.jsx
@@ -3,5 +3,10 @@ import { Link } from 'react-router-dom';
 import './navbar.css'; 
 import logo from '../../assets/logo.png';
 const Navbar = ({ isAuthenticated, onLogout }) => {
+    return (
+        <nav className="navbar">
+        
+        </nav>
+    );
     
 };
