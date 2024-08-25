bc652ad1c9a605229f829752c3aa0cc7aeaf0c5f
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 00:09:49 +0300

    imported react router dom

--- space-journey/src/pages/home/home.jsx
+++ space-journey/src/pages/home/home.jsx
@@ -6,3 +6,4 @@ import Navbar from "../../compnents/navbar/navbar.jsx";
 import AboutUs from "../../compnents/aboutus/aboutus.jsx";
 import Features from "../../compnents/features/features.jsx";
 import Footer from "../../compnents/footer/footer.jsx";
+import { useNavigate } from "react-router-dom";
