2b31eeeb1ba5f94b1dbcfb5590caef3569544b41
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-25 23:50:39 +0300

    imported redux

--- space-journey/src/pages/login/login.jsx
+++ space-journey/src/pages/login/login.jsx
@@ -2,3 +2,4 @@ import React, { useState } from "react";
 import "./login.css";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
