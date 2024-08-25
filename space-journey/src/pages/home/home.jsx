6b6f2862feb890a5c4316cc834476ccefbe4fb8f
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 00:13:34 +0300

    added Particles

--- space-journey/src/pages/home/home.jsx
+++ space-journey/src/pages/home/home.jsx
@@ -29,6 +29,92 @@ const HomePage = () => {
     };
     return (
       <div className="home-container">
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          options={{
+            fpsLimit: 60,
+            particles: {
+              number: {
+                value: 1000,
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+              },
+              color: {
+                value: "#61dbfb",
+              },
+              shape: {
+                type: "circle",
+              },
+              opacity: {
+                value: 3,
+                anim: {
+                  enable: true,
+                  speed: 2,
+                  opacity_min: 0.1,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 3,
+                anim: {
+                  enable: true,
+                  speed: 5,
+                  size_min: 0.3,
+                  sync: false,
+                },
+              },
+              move: {
+                enable: true,
+                speed: 1,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                },
+              },
+            },
+            interactivity: {
+              detectsOn: "canvas",
+              events: {
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                resize: true,
+              },
+              modes: {
+                repulse: {
+                  distance: 50,
+                  duration: 0.4,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
+            },
+            retina_detect: true,
+            background: {
+              color: "#000000",
+            },
+            fullScreen: {
+              enable: true,
+              zIndex: -1, 
+            },
+          }}
+        />
       
       </div>
     );
