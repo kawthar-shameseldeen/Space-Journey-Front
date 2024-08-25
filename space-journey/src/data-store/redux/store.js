560a3a0baa12c4fa37f360a788f3cdf2ba0244b1
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 00:52:59 +0300

    created store

--- space-journey/src/data-store/redux/store.js
+++ space-journey/src/data-store/redux/store.js
@@ -1,3 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./userSlice";
 import logger from "redux-logger";
+
+const store = configureStore({
+    reducer: {
+      users: usersReducer,
+      
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  });
