cd5e3cfad1576e5da5552de28debeac308aa6321
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 01:48:21 +0300

    fixed about us

--- space-journey/src/components/aboutus/aboutus.jsx
+++ space-journey/src/components/aboutus/aboutus.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import './aboutus.css';
+import './aboutus.css'; 
+
 const AboutUs = () => {
-    <div className="about-card">
-    <h3>About Us</h3>
-    <p>
-        Welcome to Space Journey, where space enthusiasts and students can explore the universe through immersive 3D tours and detailed information on celestial bodies. Our platform combines captivating interactive experiences with educational content, making space exploration both informative and visually stunning.
-    </p>
-</div>
-   
+    return (
+        <div className="about-card">
+            <h3>About Us</h3>
+            <p>
+                Welcome to Space Journey, where space enthusiasts and students can explore the universe through immersive 3D tours and detailed information on celestial bodies. Our platform combines captivating interactive experiences with educational content, making space exploration both informative and visually stunning.
+            </p>
+        </div>
+    );
 };
 
 export default AboutUs;
