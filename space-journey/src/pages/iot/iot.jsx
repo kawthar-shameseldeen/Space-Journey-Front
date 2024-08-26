5ec99089f7bd53bd55a1a437ce28ef9c095136dc
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 19:52:39 +0300

    added class

--- space-journey/src/pages/iot/iot.jsx
+++ space-journey/src/pages/iot/iot.jsx
@@ -3,7 +3,10 @@ import './iot.css';
 import deviceImage from '../../assets/iotImage.png';
 
 const IoTPage = () => {
-   
+    return (
+        <div className="iot-page">
+        </div>
+    );
 };
 
 export default IoTPage;
