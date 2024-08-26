17a2a86415c8c989a8ee27661aa1d08de766f383
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 20:25:04 +0300

    created protectedroutes componenet

--- space-journey/src/components/protectedRoutes/protect.jsx
+++ space-journey/src/components/protectedRoutes/protect.jsx
@@ -1,3 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+
+const ProtectedRoute = ({ children }) => {
+  
+};
+
