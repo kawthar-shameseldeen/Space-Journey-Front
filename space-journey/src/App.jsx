5189a75dfe32562879d6d1861c72e7f4088e01d5
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 22:13:02 +0300

    added routes

--- space-journey/src/App.jsx
+++ space-journey/src/App.jsx
@@ -1,53 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import Login from "./pages/login/login";
-import Signup from "./pages/signup/signup";
-import HomePage from "./pages/home/home.jsx";
-import { Provider } from "react-redux";
-import store from "./data_store/redux/store.js";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Features from "../src/components/features/features.jsx";
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   createRoutesFromElements,
-  Outlet,
-} from "react-router-dom";
+} from 'react-router-dom';
+import store from './data_store/redux/store.js';
 import ProtectedRoute from './components/protectedRoutes/protect.jsx';
-import Navbar from "./components/navbar/navbar.jsx";
-import Admin from "./pages/admin/admin.jsx";
-import AboutUs from './components/aboutus/aboutus.jsx';
-import IoTPage from './pages/iot/iot.jsx';
-import Navbar from './components/navbar/navbar.jsx';
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/features" element={<Features />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/iot" element={<IoTPage />} />
-      
-     
-    </>
-  )
-);
+import Login from './pages/login/login';
+import Signup from './pages/signup/signup';
+import Admin from './pages/admin/admin.jsx';
+import 'react-toastify/dist/ReactToastify.css';
+import HomePage from './pages/home/home.jsx';
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/home" element={<HomePage />} />
+      </>
+    )
+  );
+
   return (
-   <Provider store={store}>
-     <div className="App">
-     <ToastContainer position="top-right" />
-     <RouterProvider router={router} />
-     
-    </div>
-   </Provider>
+    <Provider store={store}>
+      <ToastContainer />
+      <RouterProvider router={router} />
+    </Provider>
   );
 }
-
 
 export default App;
