1f14730c085aa6524fe7febcf4229d9c89c61da3
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 11:40:42 +0300

    added some logic

--- space-journey/src/components/navbar/navbar.jsx
+++ space-journey/src/components/navbar/navbar.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link ,useLocation} from 'react-router-dom';
 import './navbar.css'; 
 import logo from '../../assets/logo.png';
 const Navbar = ({ isAuthenticated, onLogout }) => {
+    const location=useLocation();
+    const isAuthPage =location.pathname ==='/login' || location.pathname==='/';
+
     return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-            <img src={logo} alt="Logo" className="logo-image" />
+        <nav className="navbarB">
+            <div className="navbar-logoB">
+            <img src={logo} alt="Logo" className="logo-imageB" />
             </div>
-            <ul className="navbar-links">
+            <ul className="navbar-linksB">
                 <li><Link to="/home">Home</Link></li>
                 <li><Link to="/tour">Tour</Link></li>
                 <li><Link to="/space">Space</Link></li>
             </ul>
-            <div className="navbar-buttons">
-                {isAuthenticated ? (
-                    <button className="btn-logout" onClick={onLogout}>Logout</button>
-                ) : (
+            <div className="navbar-buttonsB">
+            {!isAuthenticated && !isAuthPage && (
                     <>
-                        <button className="btn-login"><Link to="/login">Login</Link></button>
-                        <button className="btn-signup"><Link to="/">Sign Up</Link></button>
+                        <button className="btn-loginB"><Link to="/login">Login</Link></button>
+                        <button className="btn-signupB"><Link to="/">Sign Up</Link></button>
                     </>
+                )}
+                {isAuthenticated && (
+                    <button className="btn-logoutB" onClick={onLogout}>Logout</button>
                 )}
             </div>
         </nav>
