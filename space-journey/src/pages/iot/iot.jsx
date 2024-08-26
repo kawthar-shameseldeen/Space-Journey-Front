e7a78064a1e0e9c12e5230f4dc808bedd6f355c8
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 19:53:56 +0300

    added text

--- space-journey/src/pages/iot/iot.jsx
+++ space-journey/src/pages/iot/iot.jsx
@@ -7,9 +7,13 @@ const IoTPage = () => {
         <div className="iot-page">
             <div className="iot-content">
                 <h2>Connect your IoT devices</h2>
-             
+                <p>
+                    for an interactive 3D space tour, <br />
+                    featuring real-time updates and seamless control.
+                </p>
+                
             </div>
-            
+           
         </div>
     );
 };
