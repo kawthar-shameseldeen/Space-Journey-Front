517c40f5989634de86ba34cfc8f3fe54d6733b38
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 22:54:03 +0300

    fixed bug

--- space-journey/src/pages/iot/iot.jsx
+++ space-journey/src/pages/iot/iot.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 import './iot.css'; 
 import deviceImage from '../../assets/iotImage.png';
 import Navbar from '../../components/navbar/navbar.jsx';
+
 const IoTPage = () => {
     return (
       <div>
-        <Navbar/>
-          <div className="iot-page">
-            <div className="iot-content">
-                <h2>Connect your IoT devices</h2>
-                <p>
-                    for an interactive 3D space tour, <br />
-                    featuring real-time updates and seamless control.
-                </p>
-                
-            </div>
-            <div className="iot-image">
-                <img src={deviceImage} alt="IoT Device" />
+        <Navbar />
+        <div className="iot-page">
+            <div className="iot-container">
+                <div className="iot-content">
+                    <h2>Connect your IoT devices</h2>
+                    <p>
+                        for an interactive 3D space tour, <br />
+                        featuring real-time updates and seamless control.
+                    </p>
+                    <button className="connect-button">Connect</button>
+                </div>
+                <div className="iot-image">
+                    <img src={deviceImage} alt="IoT Device" />
+                </div>
             </div>
         </div>
       </div>
