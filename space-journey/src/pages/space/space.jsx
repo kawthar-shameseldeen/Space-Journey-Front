62c74400897af3e85bbc0c9ed9754da0a0e3a442
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-29 22:59:24 +0300

    fixed bug

--- space-journey/src/pages/space/space.jsx
+++ space-journey/src/pages/space/space.jsx
@@ -3,6 +3,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { useRef, useState, useEffect } from "react";
 import axios from "axios";
+
 const SpaceTour = () => {
   const mountRef = useRef(null);
   const [currentScene, setCurrentScene] = useState("milkyWay");
@@ -11,11 +12,29 @@ const SpaceTour = () => {
   const [planetInfo, setPlanetInfo] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [planetData, setPlanetData] = useState({});
+
   useEffect(() => {
+    const fetchPlanetData = async () => {
+      try {
+        const response = await axios.get("http://localhost:4040/api/planets");
+        const planetDataArray = response.data;
+        const planetDataObj = planetDataArray.reduce((acc, planet) => {
+          acc[planet.name.toLowerCase()] = planet;
+          return acc;
+        }, {});
+        setPlanetData(planetDataObj);
+      } catch (error) {
+        console.error("Error fetching planet data:", error);
+      }
+    };
+
+    fetchPlanetData();
+
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
     mountRef.current.appendChild(renderer.domElement);
+
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
@@ -23,13 +42,16 @@ const SpaceTour = () => {
       1000
     );
     camera.position.set(0, 0, 1000);
+
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableZoom = true;
     controls.zoomSpeed = 1.5;
 
+    
     const milkyWayScene = new THREE.Scene();
     const solarSystemScene = new THREE.Scene();
     const blackholeScene = new THREE.Scene();
+
 
     const loader = new GLTFLoader();
     const textureLoader = new THREE.TextureLoader();
@@ -37,16 +59,20 @@ const SpaceTour = () => {
     const mouse = new THREE.Vector2();
 
     let blackhole = null;
+
+ 
     if (currentScene === "milkyWay") {
-      loader.load("../../assets/milky-way/scene.gltf", (gltf) => {
+      loader.load("/milky-way/scene.gltf", (gltf) => {
         const milkyWay = gltf.scene;
         milkyWay.scale.set(100, 100, 100);
         milkyWayScene.add(milkyWay);
       });
       camera.position.z = 1000;
     }
+
+  
     if (currentScene === "blackhole") {
-      loader.load("../../assets/blackhole/scene.gltf", (gltf) => {
+      loader.load("/blackhole/scene.gltf", (gltf) => {
         blackhole = gltf.scene;
         blackhole.scale.set(100, 100, 100);
         blackholeScene.add(blackhole);
@@ -54,6 +80,7 @@ const SpaceTour = () => {
       camera.position.z = 1000;
     }
 
+   
     let planets = [];
     if (currentScene === "solarSystem") {
       const ambientLight = new THREE.AmbientLight(0x404040, 5);
@@ -67,24 +94,26 @@ const SpaceTour = () => {
       const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
       const sun = new THREE.Mesh(sunGeometry, sunMaterial);
       solarSystemScene.add(sun);
+
       const createPlanet = (size, distance, texturePath, name) => {
         const geometry = new THREE.SphereGeometry(size, 32, 32);
         const texture = textureLoader.load(texturePath);
         const material = new THREE.MeshStandardMaterial({ map: texture });
         const planet = new THREE.Mesh(geometry, material);
         planet.name = name;
+
         const orbitGeometry = new THREE.RingGeometry(
           distance - 0.1,
           distance + 0.1,
           64
         );
-
         const orbitMaterial = new THREE.MeshBasicMaterial({
           color: 0x888888,
           side: THREE.DoubleSide,
         });
         const orbit = new THREE.Mesh(orbitGeometry, orbitMaterial);
         orbit.rotation.x = Math.PI / 2;
+
         planet.position.x = distance;
 
         solarSystemScene.add(orbit);
@@ -92,6 +121,7 @@ const SpaceTour = () => {
 
         return planet;
       };
+
       const createAsteroidBelt = (innerRadius, outerRadius, numAsteroids) => {
         const asteroids = [];
         for (let i = 0; i < numAsteroids; i++) {
@@ -101,17 +131,21 @@ const SpaceTour = () => {
             color: 0x888888,
           });
           const asteroid = new THREE.Mesh(asteroidGeometry, asteroidMaterial);
+
           const angle = Math.random() * 2 * Math.PI;
           const radius =
             innerRadius + Math.random() * (outerRadius - innerRadius);
           asteroid.position.x = Math.cos(angle) * radius;
           asteroid.position.z = Math.sin(angle) * radius;
           asteroid.position.y = Math.random() * 2 - 1;
+
           solarSystemScene.add(asteroid);
+
           asteroids.push(asteroid);
         }
         return asteroids;
       };
+
       const createStars = (count = 1000) => {
         for (let i = 0; i < count; i++) {
           const starGeometry = new THREE.SphereGeometry(0.5, 24, 24);
@@ -125,6 +159,7 @@ const SpaceTour = () => {
           solarSystemScene.add(star);
         }
       };
+
       planets = [
         createPlanet(1, 10, "/textures/mercury.jpg", "mercury"),
         createPlanet(1.5, 20, "/textures/venus.jpg", "venus"),
@@ -136,12 +171,16 @@ const SpaceTour = () => {
         createPlanet(5, 80, "/textures/neptune.jpg", "neptune"),
         createPlanet(5.5, 90, "/textures/pluto.jpg", "pluto"),
       ];
+
       createAsteroidBelt(40, 50, 200);
       createStars(10000);
+
       camera.position.z = 50;
     }
+
     const animate = () => {
       requestAnimationFrame(animate);
+
       switch (currentScene) {
         case "milkyWay":
           if (camera.position.z > 100) {
@@ -149,6 +188,7 @@ const SpaceTour = () => {
           }
           renderer.render(milkyWayScene, camera);
           break;
+
         case "solarSystem":
           if (isAnimating) {
             planets.forEach((planet, index) => {
@@ -160,23 +200,27 @@ const SpaceTour = () => {
           }
           renderer.render(solarSystemScene, camera);
           break;
+
         case "blackhole":
           if (blackhole) {
-            blackhole.rotation.y += 0.01;
+            blackhole.rotation.y += 0.01; 
           }
           renderer.render(blackholeScene, camera);
           break;
-          default:
-            renderer.render(milkyWayScene, camera);
-            break;
+
+        default:
+          renderer.render(milkyWayScene, camera);
+          break;
       }
     };
+
     animate();
+
     return () => {
-        mountRef.current.removeChild(renderer.domElement);
-      };
+      mountRef.current.removeChild(renderer.domElement);
+    };
   }, [currentScene, isAnimating]);
-  
+
   return (
     <div>
       <div ref={mountRef} style={{ width: "100%", height: "100vh" }} />
@@ -205,4 +249,5 @@ const SpaceTour = () => {
     </div>
   );
 };
+
 export default SpaceTour;
