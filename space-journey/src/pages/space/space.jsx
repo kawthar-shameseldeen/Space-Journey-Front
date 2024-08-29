666a886851f04a6097b57819c8cd3174af4e5e1f
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-29 21:51:13 +0300

    added useState

--- space-journey/src/pages/space/space.jsx
+++ space-journey/src/pages/space/space.jsx
@@ -5,6 +5,8 @@ import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 const SpaceTour = () => {
     const mountRef = useRef(null);
+    const [currentScene, setCurrentScene] = useState("milkyWay");
+
 
 }
 export default SpaceTour;
