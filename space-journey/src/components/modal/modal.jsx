c6b101af0a4e830e205763df3e329548bc10a123
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-30 01:31:29 +0300

    added div

--- space-journey/src/components/modal/modal.jsx
+++ space-journey/src/components/modal/modal.jsx
@@ -4,7 +4,11 @@ import "./modal.css";
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
- 
+  return (
+   <div className="modalContainer">
+    
+   </div>
+  );
 };
 
 export default Modal;
