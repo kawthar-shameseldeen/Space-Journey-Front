5f1f999d7a06453efb803f6d941fb3e745eb07a5
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-29 22:36:25 +0300

    added start geometry

--- space-journey/src/pages/space/space.jsx
+++ space-journey/src/pages/space/space.jsx
@@ -113,6 +113,7 @@ const SpaceTour = () => {
       };
       const createStars = (count = 1000) => {
         for (let i = 0; i < count; i++) {
+            const starGeometry = new THREE.SphereGeometry(0.5, 24, 24);
       
           }
        
