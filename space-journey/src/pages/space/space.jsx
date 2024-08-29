c06a8604f2b2c475431f196c8ed31c2f8ee7cce3
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-29 21:56:50 +0300

    added WebGLRenderer

--- space-journey/src/pages/space/space.jsx
+++ space-journey/src/pages/space/space.jsx
@@ -4,19 +4,15 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 const SpaceTour = () => {
-    const mountRef = useRef(null);
-    const [currentScene, setCurrentScene] = useState("milkyWay");
-    const [selectedPlanet, setSelectedPlanet] = useState(null);
-    const [isAnimating, setIsAnimating] = useState(true);
-    const [planetInfo, setPlanetInfo] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [planetData, setPlanetData] = useState({});
-    useEffect(() => {
-    
-      })
-    
-
-
-
-}
+  const mountRef = useRef(null);
+  const [currentScene, setCurrentScene] = useState("milkyWay");
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
+  const [isAnimating, setIsAnimating] = useState(true);
+  const [planetInfo, setPlanetInfo] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [planetData, setPlanetData] = useState({});
+  useEffect(() => {
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+  });
+};
 export default SpaceTour;
