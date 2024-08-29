60a43f3fe93e496d596b5f1dea12022245225fe9
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-29 22:15:03 +0300

    added mesh

--- space-journey/src/pages/space/space.jsx
+++ space-journey/src/pages/space/space.jsx
@@ -65,6 +65,7 @@ const SpaceTour = () => {
 
         const sunGeometry = new THREE.SphereGeometry(5, 32, 32);
         const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const sun = new THREE.Mesh(sunGeometry, sunMaterial);
       }
   
 
