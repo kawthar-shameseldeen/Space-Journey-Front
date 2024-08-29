91aa90e99d7c6a2ae7a5b868538a397e1d9d0182
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-29 21:47:17 +0300

    imported hooks

--- space-journey/src/pages/space/space.jsx
+++ space-journey/src/pages/space/space.jsx
@@ -1,3 +1,4 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { useRef, useState, useEffect } from "react";
