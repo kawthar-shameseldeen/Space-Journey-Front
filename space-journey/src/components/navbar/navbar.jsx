fa38e3575644167ae657d7032c663cb99a874555
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-30 14:20:50 +0300

    fixed bug

--- space-journey/src/components/navbar/navbar.jsx
+++ space-journey/src/components/navbar/navbar.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
-import { Link ,useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './navbar.css'; 
 import logo from '../../assets/logo.png';
-const Navbar = ({ isAuthenticated, onLogout }) => {
+
+const Navbar = ({ isAuthenticated, onLogout, onLoginClick, onSignupClick }) => {
     
-    const location=useLocation();
-    const isAuthPage =location.pathname ==='/login' || location.pathname==='/';
+    const location = useLocation();
+    const isAuthPage = location.pathname === '/login' || location.pathname === '/';
 
     return (
       <div className='navbarContainer'>
           <nav className="navbarB">
             <div className="navbar-logoB">
-            <img src={logo} alt="Logo" className="logo-imageB" />
+              <img src={logo} alt="Logo" className="logo-imageB" />
             </div>
             <ul className="navbar-linksB">
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/tour">Tour</Link></li>
-                <li><Link to="/space">Space</Link></li>
-                <li><Link to="/iot">Iot</Link></li>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/tour">Tour</a></li>
+                <li><a href="/space">Space</a></li>
+                <li><a href="/iot">IoT</a></li>
             </ul>
             <div className="navbar-buttonsB">
             {!isAuthenticated && !isAuthPage && (
-                    <>
-                        <button className="btn-loginB"><Link to="/login">Login</Link></button>
-                        <button className="btn-signupB"><Link to="/">Sign Up</Link></button>
-                    </>
-                )}
-                {isAuthenticated && (
-                    <button className="btn-logoutB" onClick={onLogout}>Logout</button>
-                )}
+                <>
+                    <button className="btn-loginB" onClick={onLoginClick}>Login</button>
+                    <button className="btn-signupB" onClick={onSignupClick}>Sign Up</button>
+                </>
+            )}
+            {isAuthenticated && (
+                <button className="btn-logoutB" onClick={onLogout}>Logout</button>
+            )}
             </div>
         </nav>
       </div>
     );
-    
 };
+
 export default Navbar;
