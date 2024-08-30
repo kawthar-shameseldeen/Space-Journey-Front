39f9fa0e07c6ca15d1bfa4e609991dd15440539a
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-30 19:56:10 +0300

    added another feature

--- space-journey/src/components/features/features.jsx
+++ space-journey/src/components/features/features.jsx
@@ -6,7 +6,10 @@ const featureData = [
         title: "Immersive 3D Space Tours",
         description: "Experience the universe like never before with our interactive 3D tours that let you explore planets, stars, and galaxies in stunning detail."
     },
-   
+    {
+        title: "Detailed Planetary Information",
+        description: "Dive into comprehensive data and fascinating facts about each celestial body, including their composition, atmosphere, history, and more."
+    },
    
 ];
 
