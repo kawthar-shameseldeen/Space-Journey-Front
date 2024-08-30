41e1e4f5ed840846896ec2c938e88293e578379c
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-30 19:55:45 +0300

    added features

--- space-journey/src/components/features/features.jsx
+++ space-journey/src/components/features/features.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import './features.css';
 
+const featureData = [
+    {
+        title: "Immersive 3D Space Tours",
+        description: "Experience the universe like never before with our interactive 3D tours that let you explore planets, stars, and galaxies in stunning detail."
+    },
+   
+   
+];
 
 
