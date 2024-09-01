f4369dcc1a67da53af326f924427065487efc68a
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-01 13:06:58 +0300

    added token to local storage

--- space-journey/src/pages/signup/signup.jsx
+++ space-journey/src/pages/signup/signup.jsx
@@ -31,7 +31,8 @@ const Signup = ({ onSignupSuccess }) => {
       dispatch(registerUserSuccess(response.data));
 
       const token = response.data.token;
-    
+      localStorage.setItem("token", token);
+
     } catch (error) {
       dispatch(registerUserFail(error.response.data));
       toast.error("Error signing up");
