2055ae5bf846dd42cc0ae2e9603e30ccaf0c8930
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-01 13:09:11 +0300

    removed signup route

--- space-journey/src/App.jsx
+++ space-journey/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
           }
         />
         {/* <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Signup />} /> */}
+       
         <Route path="/home" element={<HomePage />} />
        
 
