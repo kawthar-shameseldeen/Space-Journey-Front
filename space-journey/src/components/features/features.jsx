dea0c0b00a21c1ea27f093d7db8bdc42728c4ed8
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-04 00:51:57 +0300

    chanjed in features

--- space-journey/src/components/features/features.jsx
+++ space-journey/src/components/features/features.jsx
@@ -1,33 +1,32 @@
-import React from 'react';
-import './features.css';
+import React from "react";
+import "./features.css";
 
 const featureData = [
-    {
-        title: "Immersive 3D Space Tours",
-        description: "Experience the universe like never before with our interactive 3D tours that let you explore planets, stars, and galaxies in stunning detail."
-    },
-    {
-        title: "Detailed Planetary Information",
-        description: "Dive into comprehensive data and fascinating facts about each celestial body, including their composition, atmosphere, history, and more."
-    },
-   
+  {
+    title: "Immersive 3D Space Tours",
+    description:
+      "Experience the universe like never before with our interactive 3D tours that let you explore planets, stars, and galaxies in stunning detail.",
+  },
+  {
+    title: "Detailed Planetary Information",
+    description:
+      "Dive into comprehensive data and fascinating facts about each celestial body, including their composition, atmosphere, history, and more.",
+  },
 ];
 
 const Features = () => {
-    return (
-        <div className="features-section">
-           
-            <div className="features-container">
-                {featureData.map((feature, index) => (
-                    <div key={index} className="features-card">
-                        <h3>{feature.title}</h3>
-                        <p>{feature.description}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="features-section">
+      <div className="features-container">
+        {featureData.map((feature, index) => (
+          <div key={index} className="features-card">
+            <h3>{feature.title}</h3>
+            <p>{feature.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Features;
-
