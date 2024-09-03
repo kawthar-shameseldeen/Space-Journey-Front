759802d3a67d503a8c443e311e732ec2962e16f9
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-04 00:54:06 +0300

    removed routes

--- space-journey/src/App.jsx
+++ space-journey/src/App.jsx
@@ -34,17 +34,18 @@ function App() {
             </ProtectedRoute>
           }
         />
-               <Route
+               {/* <Route
           path="/space"
           element={
             <ProtectedRoute>
               <SpaceTour />
             </ProtectedRoute>
           }
-        />
+        /> */}
       
        
         <Route path="/home" element={<HomePage />} />
+        <Route path="/space" element={<SpaceTour />} />
        
 
 
