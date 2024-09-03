5cb7254220ce36ea82ca895132640a2abc6dca99
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-04 00:46:56 +0300

    changed in teh footer

--- space-journey/src/components/footer/footer.jsx
+++ space-journey/src/components/footer/footer.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import './footer.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./footer.css";
+import { Link } from "react-router-dom";
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer-top">
-                
-                <div className="footer-logo">
-                    <h2>SpaceJourney</h2>
-                </div>
-               
-            </div>
-            <div className="footer-bottom">
-                <div className="social-icons">
-                    <a href="#facebook"><i className="fab fa-facebook"></i></a>
-                    <a href="#instagram"><i className="fab fa-instagram"></i></a>
-                    <a href="#twitter"><i className="fab fa-twitter"></i></a>
-                    <a href="#whatsapp"><i className="fab fa-whatsapp"></i></a>
-                </div>
-                <p>&copy; SpaceJourney, Inc. 2024. We love our users!</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="footer-top">
+        <div className="footer-logo">
+          <h2>SpaceJourney</h2>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <div className="social-icons">
+          <a href="#facebook">
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="#instagram">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="#twitter">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#whatsapp">
+            <i className="fab fa-whatsapp"></i>
+          </a>
+        </div>
+        <p>&copy; SpaceJourney, Inc. 2024. We love our users!</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
