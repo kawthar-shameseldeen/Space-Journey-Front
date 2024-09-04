f2eb5a9e0437644274be927370cb30a97a18ba20
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-04 23:05:38 +0300

    changed width and height

--- space-journey/src/components/aboutus/aboutus.jsx
+++ space-journey/src/components/aboutus/aboutus.jsx
@@ -5,13 +5,14 @@ import spaceLottie from '../../assets/aboutus.json';
 const AboutUs = () => {
     return (
         <div className="about-card">
+             <h3>About Us</h3>
             <Player
                 autoplay
                 loop
                 src={spaceLottie} 
                 style={{ height: '200px', width: '200px', margin: '0 auto 20px' }} 
             />
-            <h3>About Us</h3>
+           
             <p>
                 Welcome to Space Journey, where space enthusiasts and students can explore the universe through immersive 3D tours and detailed information on celestial bodies. Our platform combines captivating interactive experiences with educational content, making space exploration both informative and visually stunning.
             </p>
