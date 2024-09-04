5b63c6fe1df8bab9303ec47417b282de5eac0081
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-04 23:03:11 +0300

    added player

--- space-journey/src/components/aboutus/aboutus.jsx
+++ space-journey/src/components/aboutus/aboutus.jsx
@@ -5,6 +5,12 @@ import spaceLottie from '../../assets/aboutus.json';
 const AboutUs = () => {
     return (
         <div className="about-card">
+            <Player
+                autoplay
+                loop
+                src={spaceLottie}  // Path to your Lottie animation file
+                style={{ height: '150px', width: '150px', margin: '0 auto 20px' }}  // Adjust size and margin
+            />
             <h3>About Us</h3>
             <p>
                 Welcome to Space Journey, where space enthusiasts and students can explore the universe through immersive 3D tours and detailed information on celestial bodies. Our platform combines captivating interactive experiences with educational content, making space exploration both informative and visually stunning.
