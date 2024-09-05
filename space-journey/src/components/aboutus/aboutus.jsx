d6dc944ec506d3e79e2c66715485b661dba6f035
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-05 11:23:25 +0300

    added className

--- space-journey/src/components/aboutus/aboutus.jsx
+++ space-journey/src/components/aboutus/aboutus.jsx
@@ -5,7 +5,7 @@ import spaceLottie from '../../assets/aboutus.json';
 const AboutUs = () => {
     return (
         <div className="about-card">
-             <h3>About Us</h3>
+             <h3 className='aboutTitle'>About Us</h3>
             <Player
                 autoplay
                 loop
@@ -13,8 +13,8 @@ const AboutUs = () => {
                 style={{ height: '200px', width: '200px', margin: '0 auto 20px' }} 
             />
            
-            <p>
-                Welcome to Space Journey, where space enthusiasts and students can explore the universe through immersive 3D tours and detailed information on celestial bodies. Our platform combines captivating interactive experiences with educational content, making space exploration both informative and visually stunning.
+            <p className='aboutText'>
+            Welcome to Space Journey, where space enthusiasts can explore the universe through immersive 3D tours and detailed information on celestial bodies. Our platform combines captivating interactive experiences with educational content, making space exploration both informative and visually stunning.
             </p>
         </div>
     );
