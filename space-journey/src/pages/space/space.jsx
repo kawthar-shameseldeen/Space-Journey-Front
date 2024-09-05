b26dd595ed0a182a07c54943d97cc9907f917c12
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-05 18:07:12 +0300

    imported react-icons

--- space-journey/src/pages/space/space.jsx
+++ space-journey/src/pages/space/space.jsx
@@ -3,7 +3,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { useRef, useState, useEffect } from "react";
 import { Select, MenuItem, Button } from '@mui/material';
-
+import { FaHome } from 'react-icons/fa';
 const SpaceTour = () => {
   const mountRef = useRef(null);
   const [currentScene, setCurrentScene] = useState("milkyWay");
