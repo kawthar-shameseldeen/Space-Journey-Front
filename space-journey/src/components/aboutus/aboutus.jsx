3ca3285d9e6d3199388aaa6c841e9a5b28c11e7e
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-05 11:27:34 +0300

    changed position of text

--- space-journey/src/components/aboutus/aboutus.jsx
+++ space-journey/src/components/aboutus/aboutus.jsx
@@ -5,14 +5,14 @@ import spaceLottie from '../../assets/aboutus.json';
 const AboutUs = () => {
     return (
         <div className="about-card">
-             <h3 className='aboutTitle'>About Us</h3>
+             
             <Player
                 autoplay
                 loop
                 src={spaceLottie} 
                 style={{ height: '200px', width: '200px', margin: '0 auto 20px' }} 
             />
-           
+            <h3 className='aboutTitle'>About Us</h3>
             <p className='aboutText'>
             Welcome to Space Journey, where space enthusiasts can explore the universe through immersive 3D tours and detailed information on celestial bodies. Our platform combines captivating interactive experiences with educational content, making space exploration both informative and visually stunning.
             </p>
