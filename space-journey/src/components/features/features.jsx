65af8b4306533e380b103f65c163cc2ba642e9d0
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-06 11:21:27 +0300

    fixed bug

--- space-journey/src/components/features/features.jsx
+++ space-journey/src/components/features/features.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./features.css";
-import {Player} from '@lottiefiles/react-lottie-player';
-import spaceTourLottie from '../../assets/spaceTel.json'; 
-import planetInfoLottie from '../../assets/iot.json'; 
+import { Player } from "@lottiefiles/react-lottie-player";
+import spaceTourLottie from "../../assets/spaceTel.json";
+import planetInfoLottie from "../../assets/iot.json";
 
 const featureData = [
   {
@@ -10,13 +10,13 @@ const featureData = [
     description:
       "Explore the universe with our interactive 3D space tours. Navigate through detailed planets, stars, and galaxies. Perfect for space explorers and adventurers, offering visual wonders and unforgettable experiences.",
 
-    lottieSrc: spaceTourLottie, 
+    lottieSrc: spaceTourLottie,
   },
   {
     title: "IoT-Powered Planet Lighting",
     description:
       "Experience a new level of interactivity as you explore the planets in real-time. Hover over each planet to activate IoT-driven lights, bringing them to life with a stunning visual display synced to your actions.",
-    lottieSrc: planetInfoLottie,  
+    lottieSrc: planetInfoLottie,
   },
 ];
 
@@ -26,12 +26,11 @@ const Features = () => {
       <div className="features-container">
         {featureData.map((feature, index) => (
           <div key={index} className="features-card">
-           
             <Player
               autoplay
               loop
               src={feature.lottieSrc}
-              style={{ height: '200px', width: '200px' }} 
+              style={{ height: "200px", width: "200px" }}
             />
             <h3>{feature.title}</h3>
             <p>{feature.description}</p>
