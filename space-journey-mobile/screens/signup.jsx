fc96942249d4b134bab3a19fb9089402f6ff3ff0
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-07 14:11:11 +0300

    created component

--- space-journey-mobile/screens/signup.jsx
+++ space-journey-mobile/screens/signup.jsx
@@ -1,3 +1,5 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+const SignupScreen = ({ navigation }) => {
