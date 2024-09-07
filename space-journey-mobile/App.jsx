0dcbdd7a0632027b7d49bcfbfb7f5d2450fa6e0e
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-07 13:52:40 +0300

    fixed bug

--- space-journey-mobile/App.jsx
+++ space-journey-mobile/App.jsx
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/login/login';
+import LoginScreen from './screens/login.jsx';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignupScreen from './screens/signup.jsx';
+import { NavigationContainer } from '@react-navigation/native';
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LoginScreen />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen} 
+          options={{ title: 'Sign Up' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
