0ecedce307f2f862f326c6dfe9b4852dbe4b7305
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-07 13:41:54 +0300

    imoorted react native

---
+++ space-journey-mobile/screens/login.jsx
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
