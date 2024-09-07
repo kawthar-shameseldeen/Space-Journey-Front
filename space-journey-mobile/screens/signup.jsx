de4fe70084e615b38a824f254f8640c5ab4a1244
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-07 14:12:21 +0300

    added styles

--- space-journey-mobile/screens/signup.jsx
+++ space-journey-mobile/screens/signup.jsx
@@ -73,6 +73,58 @@ const SignupScreen = ({ navigation }) => {
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 32,
+    color: '#fff',
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#aaa',
+    marginBottom: 30,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#61dbfb',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    width: '100%',
+  },
+  input: {
+    flex: 1,
+    color: '#fff',
+    paddingLeft: 10,
+    fontSize: 16,
+  },
+  signupButton: {
+    backgroundColor: '#61dbfb',
+    borderRadius: 5,
+    width: '50%',
+    padding: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  signupButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  loginText: {
+    color: '#61dbfb',
+    marginTop: 20,
+  },
 });
 
 export default SignupScreen;
