9198000a67ce87bc048bff03fdadccf3d6813278
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-12 00:55:49 +0300

    fixed addevents

--- space-journey-mobile/screens/addEvent.jsx
+++ space-journey-mobile/screens/addEvent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -6,9 +6,14 @@ import {
   TouchableOpacity,
   StyleSheet,
   Platform,
+  Alert,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from "@react-native-community/datetimepicker";
+import AsyncStorage from "@react-native-async-storage/async-storage"; 
+import {jwtDecode} from "jwt-decode"; 
+import axios from "axios";  
+import { API_URL } from '@env';  
 
 const NewEventScreen = () => {
   const [title, setTitle] = useState("");
@@ -17,14 +22,62 @@ const NewEventScreen = () => {
   const [time, setTime] = useState("");
   const [description, setDescription] = useState("");
   const [location, setLocation] = useState("");
+  const [userId, setUserId] = useState(null); 
+  const fetchUserId = async () => {
+    try {
+      const token = await AsyncStorage.getItem("token"); 
+      if (token) {
+        const decodedToken = jwtDecode(token); 
+        setUserId(decodedToken.username); 
+        console.log("User ID:", decodedToken.username); 
+      }
+    } catch (error) {
+      console.error("Error fetching token:", error);
+    }
+  };
 
-  const handleAddEvent = () => {
-    console.log("Event added:", { title, date, time, description, location });
+  
+  useEffect(() => {
+    fetchUserId();
+  }, []);
+
+  const handleAddEvent = async () => {
+    
+    const eventData = {
+      title,
+      date: date.toISOString().split("T")[0],  
+      time,
+      description,
+      location,
+      userId, 
+    };
+
+    try {
+      
+      const response = await axios.post(`${API_URL}/api/events`, eventData);
+
+    
+      console.log("Event added successfully:", response.data);
+
+      Alert.alert("Success", "Event added successfully!");
+
+      
+      setTitle("");
+      setTime("");
+      setDescription("");
+      setLocation("");
+      setDate(new Date());
+
+    } catch (error) {
+   
+      console.error("Error adding event:", error);
+      Alert.alert("Error", "Failed to add the event. Please try again.");
+    }
   };
 
   const onChangeDate = (event, selectedDate) => {
     const currentDate = selectedDate || date;
-    setShowDatePicker(Platform.OS === "ios"); 
+    setShowDatePicker(Platform.OS === "ios");
     setDate(currentDate);
   };
 
@@ -47,7 +100,7 @@ const NewEventScreen = () => {
         />
       </View>
 
-      {/* Date picker field */}
+      
       <View style={styles.inputContainer}>
         <Icon name="calendar" size={20} color="#61dbfb" style={styles.icon} />
         <TouchableOpacity onPress={showDatepicker} style={styles.dateButton}>
