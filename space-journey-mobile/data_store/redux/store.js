c147f841ef0c15665f963f6e140bb4400a56f670
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-11 20:29:13 +0300

    added redux

---
+++ space-journey-mobile/data_store/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./userSlice";
+import logger from "redux-logger";
+
+const store = configureStore({
+    reducer: {
+      users: usersReducer,
+      
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  });
+  export default store;
