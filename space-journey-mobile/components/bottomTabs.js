50649488b4c4047065761ce4fc678bae6d76c692
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-12 14:38:11 +0300

    added icon

--- space-journey-mobile/components/bottomTabs.js
+++ space-journey-mobile/components/bottomTabs.js
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/home";
 import Icon from "react-native-vector-icons/FontAwesome";
 import EventDetailsScreen from "../screens/event";
+import LiveStreamScreen from "../screens/liveStream.jsx";
+import NewEventScreen from "../screens/addEvent.jsx";
 const Tab = createBottomTabNavigator();
 export default function BottomTabs() {
   return (
@@ -15,11 +17,13 @@ export default function BottomTabs() {
           if (route.name === "Home") {
             iconName = "home";
           } else if (route.name === "Event") {
-            iconName = "cog";
+            iconName = "space-shuttle";
+          } else if (route.name === "Live") {
+            iconName = "camera";
+          } else if (route.name === "AddEvent") {
+            iconName = "calendar-o";
           }
-          else if (route.name === "Live") {
-            iconName = "cog";
-          }
+          
 
           return <Icon name={iconName} size={size} color={color} />;
         },
@@ -32,7 +36,8 @@ export default function BottomTabs() {
     >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Event" component={EventDetailsScreen} />
-      <Tab.Screen name="Live" component={EventDetailsScreen} />
+      <Tab.Screen name="Live" component={LiveStreamScreen} />
+      <Tab.Screen name="AddEvent" component={NewEventScreen} />
     </Tab.Navigator>
   );
 }
